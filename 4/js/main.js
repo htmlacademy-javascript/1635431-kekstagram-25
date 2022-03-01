4f90af8d89fb0f3a6d2b569eae9fa640f4ca2c88
Keks <keks@htmlacademy.ru>
2022-03-01 21:44:25 +0300

    :heavy_check_mark: Сборка #4

--- 4/js/main.js
+++ 4/js/main.js
@@ -1,28 +1,9 @@
-function getRandom(min , max) {
-  min = (50);
-  max = (400);
-  const GET = Math.floor(Math.random()*(max - min + 1) + min);
-  if (GET >= min && GET <=max) {
-    return GET >= 50 && GET <=300;
-  }
-  if (GET < min) {
-    return GET;
-  }
-  if (GET > max) {
-    return GET;
-  }
+function getRandomInteger(min , max) {
+  return Math.floor(Math.random()*(max - min + 1) + min);
 }
-getRandom(402);
+getRandomInteger();
 
-function checkLength(str, max) {
-  str = (90);
-  max = (80);
-  const GET = Math.floor(Math.random()*(max - str) + str);
-  if (GET > max) {
-    return 'перенос на новую строку';
-  }
-  if (GET <= max) {
-    return true;
-  }
+function checkLength (str, max) {
+  return (str > max) ? 'перенос на новую строку' : 'true';
 }
-checkLength(60);
+checkLength();
