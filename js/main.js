56493b4a976f49123ad06a10dc866d196eb81152
Алексей <90619244+Greschnik6661@users.noreply.github.com>
2022-03-01 19:05:54 +0400

    исправил замечания

--- js/main.js
+++ js/main.js
@@ -1,14 +1,29 @@
-function commentField (stringLength , numberLine) {
-  const GET = numberLine + stringLength;
-  if (stringLength >60) {
+function getRandom(min , max) {
+  min = (50);
+  max = (400);
+  const GET = Math.floor(Math.random()*(max - min + 1) + min);
+  if (GET >= min && GET <=max) {
+    return GET >= 50 && GET <=300;
+  }
+  if (GET < min) {
+    return GET;
+  }
+  if (GET > max) {
+    return GET;
+  }
+}
+getRandom(402);
+
+function checkLength(str, max) {
+  str = (90);
+  max = (80);
+  const GET = Math.floor(Math.random()*(max - str) + str);
+  if (GET > max) {
+    return 'перенос на новую строку';
+  }
+  if (GET <= max) {
     return true;
-  }
-  if (GET >150) {
-    return false;
-  }
-  if (GET <60) {
-    return false;
   }
 }
 
-commentField();
+checkLength(60);
