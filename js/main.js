741feea86ae650b1254109cc88357bdeedcfe112
Алексей <greschnik6661@mail.ru>
2022-03-22 23:53:25 +0300

    Исправил замечания

--- js/main.js
+++ js/main.js
@@ -2,13 +2,7 @@ function getRandomInteger(min , max) {
   return Math.floor(Math.random()*(max - min + 1) + min);
 }
 
-const NUMBER_OF_IMAGES_DISPLAYED = 25;
-
-const IDS = getRandomInteger(1 , 25);
-const LIKES = getRandomInteger(15 , 200);
-
-const URLS = `photos/${ getRandomInteger(1,25)  }.jpg`;
-const avatars = `img/avatar${ getRandomInteger(1,6)  }.svg`;
+const IMAGES_COUNT = 25;
 
 const names = [
   'Артём',
@@ -16,7 +10,7 @@ const names = [
   'Сергей',
   'Антон',
   'Слава',
-  'Ира'
+  'Ира',
 ];
 const messages = [
   'Всё  отлично!',
@@ -24,7 +18,7 @@ const messages = [
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
-  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 const descriptions = [
   'Нарисовааные на заборе деревья',
@@ -51,29 +45,30 @@ const descriptions = [
   'Рисунок на заборе',
   'Закат в Париже',
   'День на пляже',
-  'Красота нашего пляжа'
+  'Красота нашего пляжа',
 ];
-const randomDescriptionIndex = getRandomInteger(0, descriptions.length - 1);
-const getComments = () => {
-  const randomAvatarIndex = getRandomInteger(0, avatars.length - 1);
+
+const createComment = (_, index) => {
+  const randomNamesIndex = getRandomInteger(0, names.length - 1);
   const randomMessageIndex = getRandomInteger(0, messages.length - 1);
-  if (messages.indexOf(randomAvatarIndex) !== -1) {
-    messages.push(randomMessageIndex);
-  }
-  const randomNameIndex = getRandomInteger(0, names.length - 1);
-  if (names.indexOf(randomNameIndex) !== -1) {
-    names.push(randomNameIndex);
-  }
   return {
-    id: IDS,
-    url: URLS,
-    description: descriptions[randomDescriptionIndex],
-    likes: LIKES,
-    comments: messages,
+    id: index + 1,
+    avatar: `img/avatar${index + 1}.svg`,
+    message: messages[randomMessageIndex],
+    name: names[randomNamesIndex],
   };
 };
-getComments();
 
-const IMAGES = Array.from({length: NUMBER_OF_IMAGES_DISPLAYED}, getComments);
+const createImage = (_, index) =>
+  ({
+    id: index + 1,
+    url: `photos/${ getRandomInteger(1,25)}.jpg`,
+    description: descriptions[getRandomInteger(0, descriptions.length - 1)],
+    likes: getRandomInteger(15 , 200),
+    comments: Array.from({length: getRandomInteger}, createComment),
+  });
 
-IMAGES();
+const images = Array.from({length: IMAGES_COUNT}, createImage);
+
+// eslint-disable-next-line
+console.log(images);
