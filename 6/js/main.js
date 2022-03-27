e1be372b0beb20cfe8d8db11f98e5ae00c8d2a4c
Keks <keks@htmlacademy.ru>
2022-03-27 19:33:10 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,8 @@
+import './js_modules/util.js';
+import './js_modules/data.js';
+import './js_modules/applyingf-fects.js';
+import './js_modules/add-hash-tags.js';
+import {images} from './js_modules/download-image.js';
+
+// eslint-disable-next-line
+console.log(images);
