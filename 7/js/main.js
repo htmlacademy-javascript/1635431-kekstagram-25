10d8c07301da371fb97aed0775dcaa8409356613
Keks <keks@htmlacademy.ru>
2022-04-07 19:03:37 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,13 @@
+import './js_modules/utils.js';
+import './js_modules/picture.js';
+import './js_modules/data.js';
+import {createImage} from './js_modules/create-image.js';
+
+const IMAGES_COUNT = 25;
+
+export  {IMAGES_COUNT};
+
+const images = Array.from({length: IMAGES_COUNT}, createImage);
+// eslint-disable-next-line
+console.log(images);
+
